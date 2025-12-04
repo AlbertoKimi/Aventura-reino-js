dd6519629012e27ce4fd6a45b4a7e97fe3af332b
AlbertoKimi <breakingdawn1988@gmail.com>
2025-12-04 17:28:13 +0100

    8

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ let productosEnCesta = [];
 let enemigos = [];
 let enemigoActual = 0;
 let dinero_sobrante = 0;
+const LS_KEY_PUNTUACIONES = 'puntuacionesHistoricas';
 
 /**
  * Inicializa las clases principales (Jugador, Enemigos) y comienza el juego en la primera escena.
